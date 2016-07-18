a6403ddfccf7bca4708607467008235c05999b8a
tilden-tt <396614336@qq.com>
2016-07-18 17:05:46 +0800

    Update background.js

--- background.js
+++ background.js
@@ -2,5 +2,6 @@ chrome.webRequest.onBeforeRequest.addListener(
   function(details) { 
 	return {redirectUrl: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEgAAABIAQMAAABvIyEEAAAABlBMVEUAAABTU1OoaSf/AAAAAXRSTlMAQObYZgAAAGxJREFUeF7tyMEJwkAQRuFf5ipMKxYQiJ3Z2nSwrWwBA0+DQZcdxEOueaePp9+dQZFB7GpUcURSVU66yVNFj6LFICatThZB6r/ko/pbRpUgilY0Cbw5sNmb9txGXUKyuH7eV25x39DtJXUNPQGJtWFV+BT/QAAAAABJRU5ErkJggg=="}; 
   },
-  {urls: ["http://*/*jpg", "https://*/*jpg","http://*/*png", "https://*/*png","http://*/*gif", "https://*/*gif"]},
+  //  {urls: ["http://*/*jpg", "https://*/*jpg","http://*/*png", "https://*/*png","http://*/*gif", "https://*/*gif"]},
+  {urls: ["*://*/*/*/viewImages/*"]},
   ["blocking"])
