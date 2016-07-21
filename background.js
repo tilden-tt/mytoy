5944099926196ed37d06b9604b21be28b659e5c1
root <root@localhost.localdomain>
2016-07-21 13:46:02 +0800

    zgy test git

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+/* update by git */
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) { 
 	//return {redirectUrl: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEgAAABIAQMAAABvIyEEAAAABlBMVEUAAABTU1OoaSf/AAAAAXRSTlMAQObYZgAAAGxJREFUeF7tyMEJwkAQRuFf5ipMKxYQiJ3Z2nSwrWwBA0+DQZcdxEOueaePp9+dQZFB7GpUcURSVU66yVNFj6LFICatThZB6r/ko/pbRpUgilY0Cbw5sNmb9txGXUKyuH7eV25x39DtJXUNPQGJtWFV+BT/QAAAAABJRU5ErkJggg=="}; 
