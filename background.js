5f54673c0c1d9574dddd5456d2a6ff79c086e1c4
tilden-tt <396614336@qq.com>
2016-08-01 10:47:36 +0800

    Update background.js

--- background.js
+++ background.js
@@ -3,8 +3,8 @@ git config --global https.proxy https://proxynj.zte.com.cn:80
 git config --global http.proxy http://proxynj.zte.com.cn:80
 git clone https://github.com/tilden-tt/mytoy.git
 git config --global credential.helper store
-$ git config --global user.name "tilden-tt"
-$ git config --global  user.email 396614336@qq.com
+git config --global user.name "tilden-tt"
+git config --global  user.email 396614336@qq.com
 
 git config --global push.default simple */
 
